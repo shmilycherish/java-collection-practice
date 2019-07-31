393480c93903da5d1fd4022a0f1ec08bdecbf5b5
cyang <shmilycherish@gmail.com>
2019-07-31 14:30:35 +0800

    implement inverse reduce

--- src/main/java/com/thoughtworks/collection/InverseReduce.java
+++ src/main/java/com/thoughtworks/collection/InverseReduce.java
@@ -1,9 +1,11 @@
 package com.thoughtworks.collection;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 import java.util.List;
 import java.util.Random;
+import java.util.stream.IntStream;
+
+import static java.util.Collections.reverseOrder;
+import static java.util.stream.Collectors.toList;
 
 public class InverseReduce {
 
@@ -16,6 +18,10 @@ public class InverseReduce {
     }
 
     public List<Integer> divideToSmaller(int number) {
-        throw new NotImplementedException();
+        int divideStep = random.nextInt(3);
+        return IntStream.range(0, number).boxed()
+                .sorted(reverseOrder())
+                .filter(element -> (number - element) % divideStep == 0)
+                .collect(toList());
     }
 }
