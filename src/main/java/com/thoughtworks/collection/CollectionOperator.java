f39f107b22f9eecf3590fd5784b94137fc1a3ca5
cyang <shmilycherish@gmail.com>
2019-07-31 18:09:57 +0800

    implement collectionPerator

--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -1,31 +1,46 @@
 package com.thoughtworks.collection;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.toList;
 
 public class CollectionOperator {
     public List<Integer> getListByInterval(int left, int right) {
-        throw new NotImplementedException();
+        return Stream.concat(
+                IntStream.rangeClosed(left, right).boxed(),
+                IntStream.rangeClosed(right, left).boxed().sorted(Collections.reverseOrder()))
+                .collect(toList());
     }
 
     public List<Integer> getEvenListByIntervals(int left, int right) {
-        throw new NotImplementedException();
+        return Stream.concat(
+                IntStream.rangeClosed(left, right).boxed().filter(element -> element % 2 == 0),
+                IntStream.rangeClosed(right, left).boxed().filter(element -> element % 2 == 0).sorted(Collections.reverseOrder()))
+                .collect(toList());
     }
 
     public List<Integer> popEvenElments(int[] array) {
-        throw new NotImplementedException();
+        return stream(array).boxed().filter(element -> element % 2 == 0).collect(toList());
     }
 
     public int popLastElment(int[] array) {
-        throw new NotImplementedException();
+        List<Integer> collect = stream(array).boxed().collect(toList());
+        Collections.reverse(collect);
+        return collect.stream().findFirst().orElse(-1);
     }
 
     public List<Integer> popCommonElement(int[] firstArray, int[] secondArray) {
-        throw new NotImplementedException();
+        List<Integer> firstList = stream(firstArray).boxed().collect(toList());
+        List<Integer> secondList = stream(secondArray).boxed().collect(toList());
+        firstList.retainAll(secondList);
+        return firstList;
     }
 
     public List<Integer> addUncommonElement(Integer[] firstArray, Integer[] secondArray) {
-        throw new NotImplementedException();
+        return Stream.concat(stream(firstArray), stream(secondArray)).distinct().collect(toList());
     }
 }
