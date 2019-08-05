cc3adb340e9805d898009a4f0ddee25a645bc6c6
cyang <shmilycherish@gmail.com>
2019-08-05 15:17:59 +0800

    implement mymap

--- src/main/java/com/thoughtworks/collection/MyMap.java
+++ src/main/java/com/thoughtworks/collection/MyMap.java
@@ -1,9 +1,9 @@
 package com.thoughtworks.collection;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MyMap {
 
@@ -17,22 +17,42 @@ public class MyMap {
     }
 
     public List<Integer> getTriple() {
-        throw new NotImplementedException();
+        return array.stream().map(element -> element * 3).collect(Collectors.toList());
     }
 
     public List<String> mapLetter() {
-        throw new NotImplementedException();
+        return array.stream().map(element -> letters[element - 1]).collect(Collectors.toList());
     }
 
     public List<String> mapLetters() {
-        throw new NotImplementedException();
+        return array.stream().map(element -> {
+            String result = "";
+            int temp  = element;
+            do {
+              result = getUnit(temp) + result;
+              temp = adjustNumber(temp);
+            } while (getLettersIndex(temp) > 0);
+            return result;
+        }).collect(Collectors.toList());
     }
 
     public List<Integer> sortFromBig() {
-        throw new NotImplementedException();
+        return array.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
     }
 
     public List<Integer> sortFromSmall() {
-        throw new NotImplementedException();
+        return array.stream().sorted().collect(Collectors.toList());
+    }
+
+    private String getUnit(int element) {
+        return getLettersIndex(element) == 0 ? "z" : letters[getLettersIndex(element) - 1];
+    }
+
+    private int getLettersIndex(int element) {
+        return element % letters.length;
+    }
+
+    private int adjustNumber(int left) {
+        return getLettersIndex(left) == 0 ? left / letters.length - 1 : left / letters.length;
     }
 }
